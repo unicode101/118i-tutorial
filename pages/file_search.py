ce251a7c88e70d528e35825087244c9155e3222f
unicode101 <5075897+unicode101@users.noreply.github.com>
2024-10-29 00:58:48 -0700

    file_search

---
+++ pages/file_search.py
@@ -0,0 +1,96 @@
+#pip install --upgrade openai
+import streamlit as st
+import openai
+import os
+from openai import OpenAI, AssistantEventHandler
+ 
+client = OpenAI()
+ 
+assistant = client.beta.assistants.create(
+  name="Financial Analyst Assistant",
+  instructions="You are an expert financial analyst. Use you knowledge base to answer questions about audited financial statements.",
+  model="gpt-4o",
+  tools=[{"type": "file_search"}],
+)
+
+# Create a vector store caled "Financial Statements"
+vector_store = client.beta.vector_stores.create(name="Financial Statements")
+ 
+# Ready the files for upload to OpenAI
+file_paths = ["files/statement1.pdf", "files/statement2.txt"]
+file_streams = [open(path, "rb") for path in file_paths]
+ 
+# Use the upload and poll SDK helper to upload the files, add them to the vector store,
+# and poll the status of the file batch for completion.
+file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
+  vector_store_id=vector_store.id, files=file_streams
+)
+ 
+# You can print the status and the file counts of the batch to see the result of this operation.
+print(file_batch.status)
+print(file_batch.file_counts)
+
+assistant = client.beta.assistants.update(
+  assistant_id=assistant.id,
+  tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+)
+
+# Upload the user provided file to OpenAI
+message_file = client.files.create(
+  file=open("files/statement2.txt", "rb"), purpose="assistants"
+)
+ 
+# Create a thread and attach the file to the message
+thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+      "content": "What is the rent payment in October 2024?",
+      # Attach the new file to the message.
+      "attachments": [
+        { "file_id": message_file.id, "tools": [{"type": "file_search"}] }
+      ],
+    }
+  ]
+)
+ 
+# The thread now has a vector store with that file in its tool resources.
+print(thread.tool_resources.file_search)
+
+client = OpenAI()
+ 
+class EventHandler(AssistantEventHandler):
+    def on_text_created(self, text) -> None:
+        print(f"\nassistant > ", end="", flush=True)
+
+    def on_tool_call_created(self, tool_call):
+        print(f"\nassistant > {tool_call.type}\n", flush=True)
+
+    def on_message_done(self, message) -> None:
+        # print a citation to the file searched
+        message_content = message.content[0].text
+        annotations = message_content.annotations
+        citations = []
+        for index, annotation in enumerate(annotations):
+            message_content.value = message_content.value.replace(
+                annotation.text, f"[{index}]"
+            )
+            if file_citation := getattr(annotation, "file_citation", None):
+                cited_file = client.files.retrieve(file_citation.file_id)
+                citations.append(f"[{index}] {cited_file.filename}")
+
+        st.text(message_content.value)
+        st.text("\n".join(citations))
+
+
+# Then, we use the stream SDK helper
+# with the EventHandler class to create the Run
+# and stream the response.
+
+with client.beta.threads.runs.stream(
+    thread_id=thread.id,
+    assistant_id=assistant.id,
+    instructions="Please address the user as Jane Smith. The user has a premium account.",
+    event_handler=EventHandler(),
+) as stream:
+    stream.until_done()
